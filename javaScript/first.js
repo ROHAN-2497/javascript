788a2a8acf9cda43358b30b39b19c2ac1b2ccdf1
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-07 10:11:15 +0600

    if

--- javaScript/first.js
+++ javaScript/first.js
@@ -1,4 +1,15 @@
-let a = 5;
-let b = 2;
+// let a = 5;
+// let b = 2;
 
-console.log("a++ = ", ++a);
+// console.log("a++ = ", ++a);
+
+// Conditional Statement
+let age = 19;
+
+if (age > 18) {
+  console.log("you are too good");
+}
+
+if (age >= 20) {
+  console.log("lullu");
+}
