286c4834d1bbd01e8af350d1b80701ec497e2817
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-07 10:37:52 +0600

    kljs

--- javaScript/first.js
+++ javaScript/first.js
@@ -4,12 +4,29 @@
 // console.log("a++ = ", ++a);
 
 // Conditional Statement
-let age = 19;
+// let age = 19;
 
-if (age > 18) {
-  console.log("you are too good");
-}
+// if (age > 18) {
+//   console.log("you are too good");
+// }
 
-if (age >= 20) {
-  console.log("lullu");
+// if (age >= 20) {
+//   console.log("lullu");
+// }
+
+// let follow = "trued";
+// let rohan;
+
+// if (follow === "true") {
+//   rohan = "yes";
+// } else {
+//   rohan = "no";
+// }
+// console.log(rohan);
+
+let num = 21;
+if (num % 2 === 0) {
+  console.log(num, "even");
+} else {
+  console.log(num, "odd");
 }
