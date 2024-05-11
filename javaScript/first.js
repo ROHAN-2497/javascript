d8321c91ac6cfe4753f48945059eef08ee884f1d
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-11 07:55:06 +0600

    lk

--- javaScript/first.js
+++ javaScript/first.js
@@ -1,6 +1,11 @@
-// array
+const studentMarks = [85, 97, 44, 37, 76, 60];
 
-let marks = [12, 32, 43, 54, 543];
-for (let index = 0; index < marks.length; index++) {
-  const element = array[index];
+// create a variable for the sum and initialize it
+let sum = 0;
+
+// iterate over each item in the array
+for (let i = 0; i < studentMarks.length; i++) {
+  sum += studentMarks[i];
 }
+let average = sum / studentMarks.length;
+console.log(`avarage marks of the class ${average.toFixed()}`);
