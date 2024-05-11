dbc0c9ac582308ae23395e5def09f0ce891d3c7c
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-11 07:22:49 +0600

    kj

--- javaScript/first.js
+++ javaScript/first.js
@@ -1,5 +1,6 @@
-let fullName = prompt("Please enter your full name:");
+// array
 
-let userName = "@" + fullName + fullName.length;
-
-console.log(`Your username is: ${userName}`);
+let marks = [12, 32, 43, 54, 543];
+for (let index = 0; index < marks.length; index++) {
+  const element = array[index];
+}
